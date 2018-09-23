9b43d6d652639408441135f7214df36046c1c4f7
Monica Marziano <mmarziano1@gmail.com>
2018-09-23 23:22:19 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,7 @@ class Game
   def won?
      if WIN_COMBINATIONS.detect do |combo|
         combo[0] == combo[1] && combo[1] == combo[2]
-        combo.to_a
+        combo
       end 
       else 
         !board.full?
